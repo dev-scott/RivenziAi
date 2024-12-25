22334bd4b3394ef6431986e3f9c06133cf6c5e1d
Dev-scott <sadoscott25@gmail.com>
2024-12-25 20:05:55 +0100

    fix: desable eslint

--- src/app/(protected)/payment/page.tsx
+++ src/app/(protected)/payment/page.tsx
@@ -31,7 +31,7 @@ const Page = async ({ searchParams }: Props) => {
     return (
       <div className='flex h-screen w-full flex-col items-center justify-center'>
         <h4 className='text-5xl font-bold'>404</h4>
-        <p className='text-xl font-bold'>Oops! Something went wrong</p>
+        <p className='text-xl font-bold'>Oops!! Something went wrong</p>
       </div>
     );
   }
