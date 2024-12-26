d689d1ad8360642b7ab82698ff227893893d57e9
Dev-scott <sadoscott25@gmail.com>
2024-12-26 12:07:04 +0100

    update payment page

--- src/app/(protected)/payment/page.tsx
+++ src/app/(protected)/payment/page.tsx
@@ -2,45 +2,37 @@ import { onSubscribe } from '@/actions/user';
 import { redirect } from 'next/navigation';
 import React from 'react';
 
-// Next.js passe automatiquement les searchParams via le type PageProps
-const Page = async ({
-  searchParams,
-}: {
-  searchParams: { session_id?: string; cancel?: boolean };
-}) => {
-  const { session_id, cancel } = searchParams;
+type Props = {
+  searchParams: {
+    session_id?: string;
+    cancel?: boolean;
+  };
+};
 
+const Page = async ({ searchParams: { cancel, session_id } }: Props) => {
   if (session_id) {
     const customer = await onSubscribe(session_id);
 
     if (customer.status === 200) {
-      redirect('/dashboard');
-    } else {
-      return (
-        <div className='flex h-screen w-full flex-col items-center justify-center'>
-          <h4 className='text-5xl font-bold'>404</h4>
-          <p className='text-xl font-bold'>Oops! Something went wrong</p>
-        </div>
-      );
+      return redirect('/dashboard');
     }
-  }
 
-  if (cancel) {
     return (
       <div className='flex h-screen w-full flex-col items-center justify-center'>
         <h4 className='text-5xl font-bold'>404</h4>
-        <p className='text-xl font-bold'>Oops!! Something went wrong</p>
+        <p className='text-xl font-bold'>Oppse! Something went wrong</p>
       </div>
     );
   }
 
-  // Default fallback if neither session_id nor cancel exists
-  return (
-    <div className='flex h-screen w-full flex-col items-center justify-center'>
-      <h4 className='text-5xl font-bold'>404</h4>
-      <p className='text-xl font-bold'>Oops! Invalid Request</p>
-    </div>
-  );
+  if (cancel) {
+    return (
+      <div className='"flex w-full" h-screen flex-col items-center justify-center'>
+        <h4 className='text-5xl font-bold'>404</h4>
+        <p className='text-xl font-bold'>Oppse! Something went wrong</p>
+      </div>
+    );
+  }
 };
 
 export default Page;
