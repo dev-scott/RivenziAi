45e1a2c9c957152533b5d64e771b33462677ed00
Dev-scott <sadoscott25@gmail.com>
2025-01-05 09:49:00 +0100

    refactor app

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -38,3 +38,55 @@ export const generateTokens = async (code: string) => {
     return long_token.data;
   }
 };
+
+export const sendDM = async (
+  userId: string,
+  recieverId: string,
+  prompt: string,
+  token: string
+) => {
+  console.log('sending message');
+  return await axios.post(
+    `${process.env.INSTAGRAM_BASE_URL}/v21.0/${userId}/messages`,
+    {
+      recipient: {
+        id: recieverId,
+      },
+      message: {
+        text: prompt,
+      },
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+};
+
+export const sendPrivateMessage = async (
+  userId: string,
+  recieverId: string,
+  prompt: string,
+  token: string
+) => {
+  console.log('sending message');
+  return await axios.post(
+    `${process.env.INSTAGRAM_BASE_URL}/${userId}/messages`,
+    {
+      recipient: {
+        comment_id: recieverId,
+      },
+      message: {
+        text: prompt,
+      },
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+};
